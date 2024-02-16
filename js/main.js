b200a0af6875a6198f6220eded58d71b4d79dec3
RiazanovKS <riazanovkiril99@gmail.com>
2024-02-16 18:39:28 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+
+const Types = {
+  PLACE: 'Дворец',
+  FLAT: 'Квартира',
+  HOUSE: 'Дом',
+  BUNGALO: 'Бунгало'
+};
+
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const Features = {
+  WIFI: 'wifi',
+  DISWASHER: 'dishwasher',
+  PARKING: 'parking',
+  WASHER: 'washer',
+  ELEVATOR: 'elevator',
+  CONDITIONER: 'conditioner'
+};
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
