4a3318eb966b16f290e8258f593eee652324aecb
RiazanovKS <riazanovkiril99@gmail.com>
2024-02-27 04:10:16 +0300

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWepbackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            import: false
+                        }
+                    }],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[hash][ext]'
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[hash][ext]'
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CopyWepbackPlugin({
+            patterns: [
+                { from: "src/img", to: "img" },
+                { from: "src/fonts", to: "fonts" },
+              ],
+        }),
+        new HtmlWebpackPlugin({
+            scriptLoading: 'defer',
+            template: 'src/index.html'
+        })
+    ],
+};
